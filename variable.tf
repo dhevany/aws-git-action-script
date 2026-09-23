4254985f9bc3381ab0d6b3925cd563383023ce38
DY <80205152+dhevany@users.noreply.github.com>
2026-09-23 00:29:02 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -164,3 +164,19 @@ variable "environment" {
   type        = string
   default     = "Development"
 }
+
+# =========================================================
+# EC2 VARIABLES
+# =========================================================
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+  default     = "ami-xxxxxxxxxxxxxxxxx"
+}
